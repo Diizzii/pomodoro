25af12c10d69e2ce9e7c9876ada8c0df35010d34
Dieter Kribbel <dieter.kribbel@gmx.at>
2020-08-26 14:09:43 +0200

    Updates

---
+++ precache-manifest.6c75a5482322896968aac84596e61714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c018047aa8e9e4d7fafd347dc51ededb",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "07bfdd087932224a36d9",
+    "url": "/pomodoro/static/css/main.e26b02f7.chunk.css"
+  },
+  {
+    "revision": "597522cb13b40ad0b9b1",
+    "url": "/pomodoro/static/js/2.e0bf1524.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.e0bf1524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bfdd087932224a36d9",
+    "url": "/pomodoro/static/js/main.3ce83c89.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  }
+]);
